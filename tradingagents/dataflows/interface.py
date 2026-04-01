37f789062ad31a78eca55675919a52b3188f96b2
ultraj0510 <ultraj0510@gmail.com>
2026-04-02 02:03:04 +0900

    feat: register news_japan_rss vendor in interface

--- tradingagents/dataflows/interface.py
+++ tradingagents/dataflows/interface.py
@@ -23,6 +23,7 @@ from .alpha_vantage import (
     get_global_news as get_alpha_vantage_global_news,
 )
 from .alpha_vantage_common import AlphaVantageRateLimitError
+from .news_japan_rss import get_news_japan_rss, get_global_news_japan
 
 # Configuration and routing logic
 from .config import get_config
@@ -63,6 +64,7 @@ TOOLS_CATEGORIES = {
 VENDOR_LIST = [
     "yfinance",
     "alpha_vantage",
+    "news_japan_rss",
 ]
 
 # Mapping of methods to their vendor-specific implementations
@@ -98,10 +100,12 @@ VENDOR_METHODS = {
     "get_news": {
         "alpha_vantage": get_alpha_vantage_news,
         "yfinance": get_news_yfinance,
+        "news_japan_rss": get_news_japan_rss,
     },
     "get_global_news": {
         "yfinance": get_global_news_yfinance,
         "alpha_vantage": get_alpha_vantage_global_news,
+        "news_japan_rss": get_global_news_japan,
     },
     "get_insider_transactions": {
         "alpha_vantage": get_alpha_vantage_insider_transactions,
